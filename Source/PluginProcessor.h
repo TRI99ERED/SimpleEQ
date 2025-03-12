3c0933e50655700a2477edd9fa47c903d0ba037f
Andrii Kuchmenda <kuanwithxl@gmail.com>
2025-03-12 22:24:46 +0200

    Refactored peak coefficient generation

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -31,13 +31,16 @@ using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
 
 using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
 
-MonoChain leftChain, rightChain;
-
 enum ChainPositions {
     LowCut,
     Peak,
     HighCut,
 };
+
+using Coefficients = Filter::CoefficientsPtr;
+void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate);
 
 //==============================================================================
 /**
@@ -86,8 +89,8 @@ public:
     juce::AudioProcessorValueTreeState apvts{ *this, nullptr, "Parameters", createParameterLayout() };
 private:
     void updatePeakFilter(const ChainSettings& chainSettings);
-    using Coefficients = Filter::CoefficientsPtr;
-    static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+
+    MonoChain leftChain, rightChain;
 
     template<int Index, typename ChainType, typename CoefficientType>
     void update(ChainType& chain, const CoefficientType& coefficients) {
