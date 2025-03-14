050440b53a60a4cdd823ae464b821eae529caeb8
Andrii Kuchmenda <kuanwithxl@gmail.com>
2025-03-14 12:00:18 +0200

    Added grid lines

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -69,7 +69,7 @@ struct ResponseCurveComponent : juce::Component,
     void timerCallback() override;
 
     void paint(juce::Graphics& g) override;
-
+    void resized() override;
 private:
     SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged{ false };
@@ -77,6 +77,12 @@ private:
     MonoChain monoChain;
 
     void updateChain();
+
+    juce::Image background;
+
+    juce::Rectangle<int> getRenderArea();
+
+    juce::Rectangle<int> getAnalysisArea();
 };
 
 //==============================================================================
